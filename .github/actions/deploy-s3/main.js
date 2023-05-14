fdf3c9d778c49bac582798387e8f0f2fe22fd0b2
acharayaP03 <acharyaP03@gmail.com>
2023-05-14 19:32:03 +1000

    configure js

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -4,6 +4,14 @@ const exec = require('@actions/exec');
 
 
 function run() {
+  // 1.) get input files
+  const bucket = core.getInput('bucket', { required : true });
+  const bucketRegion = core.getInput('bucket-region' ,{ required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2.) upload files
+  const s3Url = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`)
   core.notice('Hello from my custom JavaScript Action!')
 }
 
